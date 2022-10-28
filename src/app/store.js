b35c4ee5fe16dc3e77eaaef571b11d859b0d4bd5
Ivan <nagurneaion@gmail.com>
2022-10-28 16:17:49 +0100

    add to github

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import transactionsReducer from '../features/transactions/transactionSlice';
+import budgetsReducer from '../features/budgets/budgetsSlice';
+
+export default configureStore({
+  reducer: {
+    transactions: transactionsReducer,
+    budgets: budgetsReducer,
+  },
+});
